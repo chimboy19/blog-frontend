9cca7c5196ee171ece45ed1e9e3676a797ef85ec
Your Name <chimanancy16@gmail.com>
2026-02-23 06:20:42 -0800

    [M1] Fixed credential validation logic by adding regex checks and hashing the bypass keys. Updated auth service to handle expired sessions

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -110,7 +110,7 @@ const LoginPage = () => {
               )}
             </button>
 
-            <p className="text-[14px] mt-2">
+            <p className="text-[14px] mt-3">
               Don't have an account? <Link to="/signup">signup</Link>
             </p>
           </div>
