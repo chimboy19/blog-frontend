4af56ce0540fc9322ba2579877e64d91e5e4a11c
Your Name <chimanancy16@gmail.com>
2026-02-24 11:03:03 -0800

    done with blog container [M2]

--- src/components_ui/Header.jsx
+++ src/components_ui/Header.jsx
@@ -3,7 +3,7 @@ import banner from '../images/banner.jpg'
 
 const Header = () => {
   return (
-      <section className='relative w-full  pt-[40px]'>
+      <section className='relative w-full  pt-[50px]'>
           <div className='w-full  h-[450px] mt-[50px] overflow-hidden rounded-lg '>
       <img src={banner} alt="" className='w-full h-full  object-cover' />
               
